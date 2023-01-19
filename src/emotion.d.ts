382bb7514152911139b7115ed6c892fd7750b532
dev-t2 <oo_ri@naver.com>
2023-01-19 22:52:29 +0900

    Emotion Install and Setup

---
+++ src/emotion.d.ts
@@ -0,0 +1,7 @@
+import '@emotion/react';
+
+import { CustomTheme } from './theme';
+
+declare module '@emotion/react' {
+  export interface Theme extends CustomTheme {}
+}
---
+++ src/theme.ts
@@ -0,0 +1,5 @@
+export const theme = {
+  colors: {},
+};
+
+export type CustomTheme = typeof theme;
