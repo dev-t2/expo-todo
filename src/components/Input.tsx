2dc3c632c2e1ab4c6b6b28dc8c6a8c7986dfdd24
dev-t2 <oo_ri@naver.com>
2023-01-23 10:29:03 +0900

    Input Component Style Update

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,32 +11,43 @@ const Container = styled.View({
 
 interface ITitle {
   isFocused: boolean;
+  value: string;
 }
 
-const Title = styled.Text<ITitle>(({ theme, isFocused }) => ({
-  color: isFocused ? theme.colors.primary.default : theme.colors.gray[500],
-  fontWeight: isFocused ? '600' : '500',
+const Title = styled.Text<ITitle>(({ theme, isFocused, value }) => ({
+  color: isFocused
+    ? theme.colors.primary.default
+    : value
+    ? theme.colors.black
+    : theme.colors.gray[500],
+  fontWeight: isFocused || value ? '600' : '500',
 }));
 
 interface IStyledTextInput {
   isFocused: boolean;
+  value: string;
 }
 
-const StyledTextInput = styled.TextInput<IStyledTextInput>(({ theme, isFocused }) => ({
+const StyledTextInput = styled.TextInput<IStyledTextInput>(({ theme, isFocused, value }) => ({
   height: 40,
   paddingHorizontal: 10,
-  borderWidth: isFocused ? 2 : 1,
+  borderWidth: isFocused || value ? 2 : 1,
   borderRadius: 8,
-  borderColor: isFocused ? theme.colors.primary.default : theme.colors.gray[500],
+  borderColor: isFocused
+    ? theme.colors.primary.default
+    : value
+    ? theme.colors.black
+    : theme.colors.gray[500],
   marginTop: 4,
-  color: isFocused ? theme.colors.black : theme.colors.gray[500],
+  color: isFocused || value ? theme.colors.black : theme.colors.gray[500],
 }));
 
 interface IInput extends TextInputProps {
   title: string;
+  value: string;
 }
 
-const Input: FC<IInput> = ({ title, ...props }) => {
+const Input: FC<IInput> = ({ title, value, ...props }) => {
   const theme = useTheme();
 
   const [isFocused, setIsFocused] = useState(false);
@@ -51,7 +62,9 @@ const Input: FC<IInput> = ({ title, ...props }) => {
 
   return (
     <Container>
-      <Title isFocused={isFocused}>{title}</Title>
+      <Title isFocused={isFocused} value={value}>
+        {title}
+      </Title>
 
       <StyledTextInput
         {...props}
@@ -60,6 +73,7 @@ const Input: FC<IInput> = ({ title, ...props }) => {
         textContentType="none"
         placeholderTextColor={theme.colors.gray[500]}
         isFocused={isFocused}
+        value={value}
         onFocus={onFocus}
         onBlur={onBlur}
       />
